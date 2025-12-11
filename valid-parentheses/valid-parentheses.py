41e60b855bbbab326c97e80fe8a570cc7fbf3a3f
macrespo42 <macrespo@student.42.fr>
2025-12-11 14:41:30 +0100

    feat(py): add python solution to valid-parentheses

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+def is_valid(s: str):
+    stack = []
+    open = ["(", "[", "{"]
+    closed = [")", "]", "}"]
+    for elt in s:
+        if (elt in open):
+            stack.append(elt)
+        elif elt in closed:
+            popped = stack.pop()
+            if (closed.index(elt) != open.index(popped)):
+                return False
+    return len(stack) == 0
+
+if __name__ == "__main__":
+    print(is_valid("()")) # True
+    print(is_valid("()[]{}")) # True
+    print(is_valid("(]")) # False
+    print(is_valid("({})")) # True
+    print(is_valid("([)]")) # False
